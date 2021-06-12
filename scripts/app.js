6ab5ed35731395903ed1ff082b59c5fb3b8e8e13
nayyyhaa <nehaguptang.ng@gmail.com>
2021-06-12 23:09:13 +0530

    feat: scroll animation

--- scripts/app.js
+++ scripts/app.js
@@ -27,7 +27,34 @@ function animateScroll() {
         slideT1.fromTo(revealImg, {x: "0%"}, {x: "100%"});
         slideT1.fromTo(img, {scale: 2}, {scale: 1}, "-=1");
         slideT1.fromTo(revealText, {x: "0%"}, {x: "100%"}, "-=0.75");
-        slideT1.fromTo(navBar, {y: "-100%"}, {y:"0%"}, "-=0.9")
+        slideT1.fromTo(navBar, {y: "-100%"}, {y:"0%"}, "-=0.9");
+
+        /*add scene*/ 
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse: false
+        })
+        .setTween(slideT1)
+        .addIndicators({colorStart: 'white', colorTrigger: 'white', name: 'slide'})
+        .addTo(controller);
+
+        /*new animation*/  
+        const pageT1 = gsap.timeline();
+
+        pageT1.fromTo(slide, {opacity: 1, scale: 1}, {opacity: 0, scale: 0.1});
+
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: "100%",
+            triggerHook: 0
+        })
+        .setTween(pageT1)
+        .setPin(slide, {pushFollowers: false})
+        .addIndicators({colorStart: 'white', colorTrigger: 'white', name: 'page', indent: 200})
+        .addTo(controller);
+
+
     })
 }
 
